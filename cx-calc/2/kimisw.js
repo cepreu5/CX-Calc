4d2575c604453ec81cf2e740ba258bbdacefac31
cepreu5 <cepreu5@live.com>
2025-09-09 00:53:39 +0300

    mp3 version

--- cx-calc/2/kimisw.js
+++
@@ -1,3 +0,0 @@
-// празен service-worker – просто регистриран, за да удовлетвори PWA
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
