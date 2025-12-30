e7189eacdcdaffa49256fa270b92c9d221ca31fd
cepreu5 <cepreu5@live.com>
2025-12-30 04:43:37 +0200

    ver 1.9 Resto стотинки ок

--- cx-calc/sww.js
+++ cx-calc/sww.js
@@ -1,5 +1,5 @@
 // sw.js – Service Worker за CX-Calc (PWA + офлайн)
-const CACHE_NAME = 'cx-calc-1.8'; // Версия на кеша, редактирай при промяна на ресурсите
+const CACHE_NAME = 'cx-calc-1.9'; // Версия на кеша, редактирай при промяна на ресурсите
 // Важно: промени версията при всяка промяна на кешираните ресурси!
 // Това ще принуди браузъра да изтегли новия кеш и да активира новия SW.
 const OFFLINE_PAGE = new URL('index.html', self.location).href;
@@ -9,8 +9,6 @@ const ASSETS = [
   OFFLINE_PAGE,
   'index.html',
   'style.css',
-  'resto.css',
-  'resto.js',
   'mainnAll.js',
   'click.wav',
   'Calculator0.png',
